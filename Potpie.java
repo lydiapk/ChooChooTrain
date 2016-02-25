b6a9e56bacb0ea5efdaec73e712f9685542852ff
lydiapk <lydiapk15@gmail.com>
2016-02-25 10:36:51 -0500

    Added files via upload

---
+++ Potpie.java
@@ -0,0 +1,11 @@
+public class Potpie extends Food
+{
+    public void eat()
+    {
+        System.out.println("Potpies are yum-yum!");
+    }
+    public void make()
+    {
+        System.out.println("Put the potpie in the oven.");
+    }
+}
